0b30753d4f3946ed6afbd51fbcc18462e4055ccb
Maksim0302 <bilykmaksim06@gmail.com>
2022-06-05 19:52:20 +0300

    github-for
    github-for

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// $('.reviews-slider').slick({
+//     centerMode: true,
+//     centerPadding: '60px',
+//     slidesToShow: 3,
+//     responsive: [
+//       {
+//         breakpoint: 768,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 3
+//         }
+//       },
+//       {
+//         breakpoint: 480,
+//         settings: {
+//           arrows: false,
+//           centerMode: true,
+//           centerPadding: '40px',
+//           slidesToShow: 1
+//         }
+//       }
+//     ]
+//   });
+
+
+
+
+
+$('.reviews-slider').slick({
+  centerMode: true,
+  slidesToShow: 1,
+  variableWidth: true,
+  arrows: false,
+  swipe: true,
+  dots: true
+});
+
